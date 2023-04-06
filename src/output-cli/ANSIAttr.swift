5c370ed7b1a9cd0c5b7669afd555bd0e8bae9437
Aaron J <am@jonesiscoding.com>
2023-04-06 08:44:36 -0500

    output-cli code

---
+++ src/output-cli/ANSIAttr.swift
@@ -0,0 +1,104 @@
+//
+// Created by Aaron Jones on 9/30/22.
+//
+
+import Foundation
+import Commands
+
+public enum ANSIAttr: UInt8 {
+    /* text styling */
+    case normal         = 0
+    case bold           = 1
+    case dim            = 2
+    case italic         = 3
+    case underline      = 4
+    case blink          = 5
+    case overline       = 6
+    case inverse        = 7
+    case hidden         = 8
+    case strike         = 9
+    case noBold         = 21
+    case noDim          = 22
+    case noItalic       = 23
+    case noUnderline    = 24
+    case noBlink        = 25
+    case noOverline     = 26
+    case noInverse      = 27
+    case noHidden       = 28
+    case noStrike       = 29
+    /* foreground text coloring */
+    case black          = 30
+    case red            = 31
+    case green          = 32
+    case brown          = 33
+    case blue           = 34
+    case magenta        = 35
+    case cyan           = 36
+    case gray           = 37
+    case fore256Color   = 38
+    case `default`      = 39
+    case darkGray       = 90
+    case lightRed       = 91
+    case lightGreen     = 92
+    case yellow         = 93
+    case lightBlue      = 94
+    case lightMagenta   = 95
+    case lightCyan      = 96
+    case white          = 97
+    /* background text coloring */
+    case onBlack        = 40
+    case onRed          = 41
+    case onGreen        = 42
+    case onBrown        = 43
+    case onBlue         = 44
+    case onMagenta      = 45
+    case onCyan         = 46
+    case onGray         = 47
+    case back256Color   = 48
+    case onDefault      = 49
+    case onDarkGray     = 100
+    case onLightRed     = 101
+    case onLightGreen   = 102
+    case onYellow       = 103
+    case onLightBlue    = 104
+    case onLightMagenta = 105
+    case onLightCyan    = 106
+    case onWhite        = 107
+}
+
+public let ESC = "\u{1B}"  // Escape character (27 or 1B)
+public let CSI = ESC+"["   // Control Sequence Introducer
+
+public extension String {
+    func style(_ aStyle: ANSIAttr) -> String {
+        guard !isEmpty else {
+            return self
+        }
+
+        if aStyle == .normal {
+            return CSI + "\(aStyle.rawValue)m" + self
+        } else {
+            return CSI + "\(aStyle.rawValue)m" + self + CSI + "\(ANSIAttr.normal.rawValue)m"
+        }
+    }
+
+    func message() -> String {
+        return style(.magenta)
+    }
+
+    func success() -> String {
+        return style(.green)
+    }
+
+    func error() -> String {
+        return style(.red)
+    }
+
+    func warning() -> String {
+        return style(.yellow)
+    }
+
+    func info() -> String {
+        return style(.cyan)
+    }
+}
---
+++ src/output-cli/Output.swift
@@ -0,0 +1,201 @@
+//
+// Created by Aaron Jones on 9/30/22.
+//
+
+import Foundation
+import Commands
+
+public enum OutputContext: String, CaseIterable {
+    case notify = "notify"
+    case success = "success"
+    case error = "error"
+    case message = "msg"
+    case info = "info"
+    case normal = "default"
+}
+
+public class Output {
+    public var section: Int = 0
+    public var level: Int
+    private let line: String = "----------------------------------------------------------------------------"
+    public var notifying: Int = 0
+    private lazy var color: Bool = {
+        let result = Commands.Bash.run("[ -n \"$TERM\" ] && [ \"$TERM\" != \"dumb\" ] && /usr/bin/tput -T$TERM colors")
+        if result.isFailure {
+          return false
+        } else if result.output.isEmpty {
+          return false
+        } else if 8 >= Int(result.output) ?? 0 {
+          return false
+        }
+        return true
+    }()
+
+    public init(level: Int) {
+        self.level = level
+    }
+    
+    // Contextual Functions
+    
+    public func successbg(_ badge: String) {
+        self.badge(badge: badge, context: .success)
+    }
+    
+    public func errorbg(_ badge: String) {
+        self.badge(badge: badge, context: .error)
+    }
+
+    public func normalbg(_ badge: String) {
+        self.badge(badge: badge)
+    }
+    
+    public func error(_ message: String, level: Int = 1) {
+        self.output(message: message, context: .error, level: level)
+    }
+
+    public func info(_ message: String, level: Int = 1) {
+        self.output(message: message, context: .info, level: level)
+    }
+
+    public func msg(_ message: String, level: Int = 1) {
+        self.output(message: message, context: .message, level: level)
+    }
+
+    public func normal(_ message: String, level: Int = 1) {
+        self.output(message: message, context: .normal, level: level)
+    }
+
+    public func success(_ message: String, level: Int = 1) {
+        self.output(message: message, context: .success, level: level)
+    }
+
+    public func errorln(_ message: String, level: Int = 1) {
+        self.outputln(message: message, context: .error, level: level)
+    }
+
+    public func infoln(_ message: String, level: Int = 1) {
+        self.outputln(message: message, context: .info, level: level)
+    }
+
+    public func msgln(_ message: String, level: Int = 1) {
+        self.outputln(message: message, context: .message, level: level)
+    }
+
+    public func normalln(_ message: String, level: Int = 1) {
+        self.outputln(message: message, context: .normal, level: level)
+    }
+
+    public func successln(_ message: String, level: Int = 1) {
+        self.outputln(message: message, context: .success, level: level)
+    }
+    
+    // Utility Functions
+    
+    public func done() {
+        successbg("DONE")
+    }
+
+    public func quiet() {
+        self.level = -1
+    }
+
+    public func verbosity(level: Int) {
+        self.level = level
+    }
+
+    public func section(message: String = "Section") {
+        self.msgln(message)
+        self.section = self.section + 1
+    }
+
+    public func endSection() {
+      self.section = self.section - 1
+      if self.section < 0 {
+        self.section = 0
+      }
+    }
+
+    public func notify(message: String, level: Int = 1) {
+        if self.level >= level {
+            self.notifying = level
+            let output = self.color ? message.style(.cyan) : message
+            pr(output.padding(toLength: (76 - (self.section * 2)), withPad: ".", startingAt: 0), terminator: " ")
+        }
+    }
+    
+    // Generic Functions
+    
+    public func badge(badge: String, context: OutputContext = .normal) {
+        if self.level >= self.notifying && self.notifying > 0 {
+            var output: String = badge
+            if(self.color) {
+                switch(context) {
+                case OutputContext.success:
+                    output = badge.style(.green)
+                case OutputContext.error:
+                    output = badge.style(.red)
+                case OutputContext.message:
+                    output = badge.style(.magenta)
+                case OutputContext.info:
+                    output = badge.style(.cyan)
+                default:
+                    break
+                }
+            }
+            print("[\(output)]")
+            self.notifying = 0
+        }
+    }
+    
+    public func output(message: String, context: OutputContext = .normal, level: Int = 1, terminator: String = " ") {
+        if self.notifying >= self.level {
+            switch(context) {
+            case OutputContext.success:
+                self.badge(badge: "SUCCESS", context: .success)
+            case OutputContext.error:
+                self.badge(badge: "ERROR", context: .error)
+            case OutputContext.message:
+                self.badge(badge: "SEE BELOW", context: .message)
+            case OutputContext.info:
+                self.badge(badge: "SEE BELOW", context: .info)
+            default:
+                self.badge(badge: "SEE BELOW", context: .normal)
+            }
+        }
+
+        if self.level >= level {
+            var output: String = message
+            if(self.color) {
+                switch(context) {
+                case OutputContext.success:
+                    output = message.style(.green)
+                case OutputContext.error:
+                    output = message.style(.red)
+                case OutputContext.message:
+                    output = message.style(.magenta)
+                case OutputContext.info:
+                    output = message.style(.cyan)
+                default:
+                    break
+                }
+            }
+            
+            pr(output, separator: " ", terminator: terminator)
+        }
+    }
+    
+    public func outputln(message: String, context: OutputContext = .normal, level: Int = 1) {
+        self.output(message: message, context: context, level: level, terminator: "\n")
+    }
+    
+    // Private Functions
+
+    private func pr(_ string: String, separator: String = " ", terminator: String = "\n") {
+        if self.section > 0 {
+          let repeated = String(repeating: " ", count: (self.section * 2))
+          print(repeated, terminator: "")
+        }
+
+        print(string, separator: separator, terminator: terminator)
+    }
+}
